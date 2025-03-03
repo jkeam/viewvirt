5a7f07551f754fe15d9e2912a1410b7001220b76
Jon Keam <jpkeam@gmail.com>
2025-03-02 21:57:20 -0500

    update hash and build files

--- api/build.sh
+++ api/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-podman build -t quay.io/jkeam/viewvirt -f ./Dockerfile . && podman push quay.io/jkeam/viewvirt
+podman build -t quay.io/jkeam/viewvirt-api -f ./Dockerfile . && podman push quay.io/jkeam/viewvirt-api
--- web/build.sh
+++ web/build.sh
@@ -3,4 +3,4 @@
 npm install
 npm run clean
 npm run build
-podman build -t quay.io/jkeam/viewvirt -f ./Dockerfile . && podman push quay.io/jkeam/viewvirt
+podman build -t quay.io/jkeam/viewvirt-web -f ./Dockerfile . && podman push quay.io/jkeam/viewvirt-web
